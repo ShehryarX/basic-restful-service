496d1382885e3176cce898e7ad744a677aed2ef4
Shehryar Assad <xshehryar@gmail.com>
2020-01-11 21:31:14 -0500

    Add boilerplate

---
+++ src/main/java/ca/shehryar/mobileapprestfulws/MobileAppRestfulWsApplication.java
@@ -0,0 +1,13 @@
+package ca.shehryar.mobileapprestfulws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppRestfulWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileAppRestfulWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/shehryar/mobileapprestfulws/MobileAppRestfulWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.shehryar.mobileapprestfulws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppRestfulWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
