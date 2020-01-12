9bfa7c877c5b12d49dc2ac23b8664b0747ad95ec
Shehryar Assad <xshehryar@gmail.com>
2020-01-12 13:16:31 -0500

    Add duplicate entry validation

--- src/main/java/ca/shehryar/mobileapprestfulws/service/impl/UserServiceImpl.java
+++ src/main/java/ca/shehryar/mobileapprestfulws/service/impl/UserServiceImpl.java
@@ -1,13 +1,36 @@
 package ca.shehryar.mobileapprestfulws.service.impl;
 
+import ca.shehryar.mobileapprestfulws.UserRepository;
+import ca.shehryar.mobileapprestfulws.io.entity.UserEntity;
 import ca.shehryar.mobileapprestfulws.service.UserService;
 import ca.shehryar.mobileapprestfulws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Record already exists");
+        }
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedDetails = userRepository.save(userEntity);
+
+        UserDto returnVal = new UserDto();
+        BeanUtils.copyProperties(storedDetails, returnVal);
+
+        return returnVal;
     }
 }
