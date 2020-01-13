8778b829424480df3a76124eef765aa97a52d40f
Shehryar Assad <xshehryar@gmail.com>
2020-01-12 23:33:13 -0500

    Read secret token from app properties

--- src/main/java/ca/shehryar/mobileapprestfulws/MobileAppRestfulWsApplication.java
+++ src/main/java/ca/shehryar/mobileapprestfulws/MobileAppRestfulWsApplication.java
@@ -1,5 +1,6 @@
 package ca.shehryar.mobileapprestfulws;
 
+import ca.shehryar.mobileapprestfulws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,4 +22,10 @@ public class MobileAppRestfulWsApplication {
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
     }
+
+    @Bean(name = "AppProperties")
+    public AppProperties getAppProperties() {
+        return new AppProperties();
+    }
 }
+w
--- src/main/java/ca/shehryar/mobileapprestfulws/security/SecurityConstants.java
+++ src/main/java/ca/shehryar/mobileapprestfulws/security/SecurityConstants.java
@@ -1,9 +1,15 @@
 package ca.shehryar.mobileapprestfulws.security;
 
+import ca.shehryar.mobileapprestfulws.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_DATE = 864000000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "heresmysecret";
+
+    public static String getTokenSecret() {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
